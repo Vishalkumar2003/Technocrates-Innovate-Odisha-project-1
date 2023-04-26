1af238772990cdfdaa33110adbf5003b375833e4
vishal <vishalkumarnke93@gmail.com>
2023-04-27 01:12:40 +0530

    innovate odisha

---
+++ website code/index.js
@@ -0,0 +1,9 @@
+// function getRandomInt(max,range) {
+//     return Math.floor((Math.random() * max)+range);
+// }
+
+// setInterval(function(){
+//     console.log(getRandomInt(11,55));
+//     console.log(getRandomInt(2,6.5));
+//     console.log(getRandomInt(20,100));
+// },800);
